1e396e066cc485dcd7db4d7e23f8175b0e21b0ee
chimbise <58652155+chimbise@users.noreply.github.com>
2024-05-21 12:38:00 +0000

    sads

--- configure.js
+++ configure.js
@@ -1,6 +1,5 @@
 import { myVariable } from './index.js';
 
-
 const configList = document.getElementById("leftSide");
 const liElement = configList.getElementsByTagName("li");
 
@@ -55,8 +54,6 @@ function handleLiClicks(li) {
             break;
     }
 }
-
-
 // Function to create a new building list item
 function createParentItem(doc) {
     var listItem = document.createElement('li');
@@ -83,8 +80,6 @@ function createParentItem(doc) {
 
     return listItem;
 }
-
-
 // Function to create a new room list item
 function createChildItem(key,doc,build) {
     var buildRoom = build+'#'+key                 //separate string with # for easy extraction when getting data
@@ -100,16 +95,19 @@ function createChildItem(key,doc,build) {
    
     return listItem;
 }
+var mainObject = {}
+var parentObject = {};
+var childObject = {};
+var grandChildObject = {};
 // Function to create a new question list item
 function createGrandChildItem(key,questionObject,buildRoom) {
-    
+
     var listItem = document.createElement('li');
     const p = document.createElement('p');
     p.textContent = key;
 
-    const radio1 = createRadioButton(buildRoom+key+'comment-only' , buildRoom+key, 'comment-only',questionObject);
-    const radio2 = createRadioButton(buildRoom+key+'comment-with-yes', buildRoom+key, 'comment-with-yes',questionObject);
-
+    const radio1 = createRadioButton(buildRoom+key+'comment-only' ,key, 'comment-only',questionObject,buildRoom);
+    const radio2 = createRadioButton(buildRoom+key+'comment-with-yes', key, 'comment-with-yes',questionObject, buildRoom);
     
     // Append elements
     listItem.appendChild(p);
@@ -122,24 +120,47 @@ function createGrandChildItem(key,questionObject,buildRoom) {
     listItem.classList.add('configQuestion');
     return listItem;
 }
+
     // Function to create radio button
-    function createRadioButton(id, name, value,questionObject) {
+    function createRadioButton(id, name, value,questionObject, buildRoom) {
 
         const input = document.createElement('input');
         input.type = 'radio';
-        input.name = name;
+        input.name = name + buildRoom;  //unique to other rooms but similar in the same room.
         input.value = value;
         input.id = id;
 
+        let words = buildRoom.split('#');
+        let building = words[0];
+        let room = words[1];
+
         if(questionObject[name] === '1' && value === 'comment-with-yes'){
             input.checked = true;
+
+            grandChildObject[name] = '1';;
+            childObject[room] = grandChildObject;
         }
         
         if(questionObject[name] === '2' && value === 'comment-only'){
             input.checked = true;
+
+            grandChildObject[name] = '2';;
+            childObject[room] = grandChildObject;
         }
-    
-        
+        console.log(childObject);
+        // Add event listener to capture the change event
+        input.addEventListener('change', function() {
+            if (input.checked) {
+                console.log('Selected value:', input.value);
+                if(input.value === 'comment-only' ){
+                    building[room][name] = '2'
+                }else{
+                    building[room][name] = '1'
+                }
+                // You can also perform other actions here based on the changed value
+            } 
+            //console.log(mainObject);
+        });
 
         const label = document.createElement('label');
         label.appendChild(input);
@@ -154,9 +175,6 @@ function createGrandChildItem(key,questionObject,buildRoom) {
 
         return label;
     }
-
-
-
 // Function to remove the list item
 function removeListItem(element) {
     var listItem = element.parentNode;
@@ -220,7 +238,6 @@ document.getElementById('add-new-building').onclick = function() {
         document.getElementById("addNewBuildingForm").style.display = "none";
     });
 // Function to add a new child list item
-
 function addChildListItem(doc) {
     
     var sublist = document.createElement('ul');
@@ -253,7 +270,6 @@ function updateRoomUl(roomObject,docId){
         }
       }
 }
-
 // Function to add a new child list item
 function addGrandChildListItem(questionObject,buildRoom) {
     var sublist2 = document.createElement('ul');
@@ -272,7 +288,7 @@ function addGrandChildListItem(questionObject,buildRoom) {
     }
     return sublist2
 }
-    // Function to add a new child list item
+// Function to add a new child list item
 function updateQuestionUl(questionObject,buildRoom){
     var questionUl = document.getElementById(buildRoom)
     for (let key in questionObject) {
@@ -351,33 +367,30 @@ function addAddWidgetGrand(childItem, buildRoomx) {
     childItem.appendChild(addWidget);
 }
 
-    document.getElementById("textInputForm2").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent the default form submission
+document.getElementById("textInputForm2").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
 
-        
-        var inputValue = document.getElementById("textInput2").value;
-        // Get the value of the text input
-        if (inputValue === null || inputValue.trim() === '') {
-            // Exit if the user cancels or provides an empty question
-            return;
-            }
-        // Display the input value (You can perform any action here)
-        console.log("User input:", inputValue);
-        var data  =  {
-            [inputValue]: '1'
+    
+    var inputValue = document.getElementById("textInput2").value;
+    // Get the value of the text input
+    if (inputValue === null || inputValue.trim() === '') {
+        // Exit if the user cancels or provides an empty question
+        return;
         }
+    // Display the input value (You can perform any action here)
+    console.log("User input:", inputValue);
+    var data  =  {
+        [inputValue]: '1'
+    }
 
-        updateQuestionUl(data,buildRoom)
-        // Clear the input field
-        document.getElementById("textInput2").value = "";
+    updateQuestionUl(data,buildRoom)
+    // Clear the input field
+    document.getElementById("textInput2").value = "";
 
-        // Hide the form after submission
-        document.getElementById("textInputForm2").style.display = "none";
-    });
-
-
+    // Hide the form after submission
+    document.getElementById("textInputForm2").style.display = "none";
+});
 addParentListItem(myVariable)
-
 function toggleVisibility(ul) {
     var ulElement = ul.querySelector('.configBuilding > ul');
     if (ulElement.style.maxHeight === '0px' || ulElement.style.maxHeight === '') {
@@ -386,13 +399,10 @@ function toggleVisibility(ul) {
         ulElement.style.maxHeight = '0px';
     }
   }
-
-
 document.getElementById('save').onclick = function() {
-
     getData();
 }
 function getData(){
 
-
+    
 }
